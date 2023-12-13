4ab64f119e2bf4e3f4be7ff9b5e4db2c1ce652a8
unknown <tobaraschristopher@yahoo.ie>
2023-12-13 15:10:28 +0000

    Added hour and minute check instead

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,12 +2,18 @@ class Schedule < ApplicationRecord
     validates :origin, presence: true
     validates :destination, presence: true
     validates :schetime, presence: true
-    #validate :schetime_cannot_be_in_the_past
+    validate :schetime_cannot_be_in_the_past
 
-    #private
+    private
 
-    #def schetime_cannot_be_in_the_past
-#errors.add(:schetime, 'cannot be in the past') if schetime.present? && schetime < Time.current
-    #end
-
-end
+    def schetime_cannot_be_in_the_past
+      return unless schetime.present?
+  
+      current_time = Time.current
+      schetime_time = schetime.to_time
+  
+      if schetime_time.hour < current_time.hour || (schetime_time.hour == current_time.hour && schetime_time.min <= current_time.min)
+        errors.add(:schetime, 'cannot be in the past')
+      end
+    end
+  end
