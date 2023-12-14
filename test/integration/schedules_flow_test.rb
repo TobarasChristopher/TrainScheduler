85220f940546bf5e18625c8b9b683547ca6c9bc5
unknown <tobaraschristopher@yahoo.ie>
2023-12-14 11:06:58 +0000

    reintroduced broken tests

--- test/integration/schedules_flow_test.rb
+++ test/integration/schedules_flow_test.rb
@@ -1,18 +1,18 @@
-#require 'test_helper'
-#
-#class SchedulesFlowTest < ActionDispatch::IntegrationTest
-#  test "can create a schedule" do
-#    get "/schedules/new"
-#    get schedule_path(schedule.id)
-#    assert_response :success
-#
-#    assert_difference('Schedule.count') do
-#      post schedules_path, params: { schedule: { origin: "New Origin", destination: "New destination", schetime: DateTime.parse("2023-12-13 14:30:00") } }
-#    end
-#
-#    assert_redirected_to schedule_path(assigns(:schedule))
-#    follow_redirect!
-#
-#    assert_select 'p', 'Origin: New Origin'
-#  end
-#end
+require 'test_helper'
+
+class SchedulesFlowTest < ActionDispatch::IntegrationTest
+  test "can create a schedule" do
+    get "/schedules/new"
+    get schedule_path(schedule.id)
+    assert_response :success
+
+    assert_difference('Schedule.count') do
+      post schedules_path, params: { schedule: { origin: "New Origin", destination: "New destination", schetime: DateTime.parse("2023-12-13 14:30:00") } }
+    end
+
+    assert_redirected_to schedule_path(assigns(:schedule))
+    follow_redirect!
+
+    assert_select 'p', 'Origin: New Origin'
+  end
+end
