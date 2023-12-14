53a633601dd31a0fc0fd597c42d3db82a97051b3
unknown <tobaraschristopher@yahoo.ie>
2023-12-14 10:52:46 +0000

    reset previous commits

---
+++ app/assets/javascripts/alerts.js
@@ -0,0 +1,21 @@
+// app/assets/javascripts/alerts.js
+$(document).ready(function() {
+    setInterval(function() {
+      $.ajax({
+        url: '/schedules/expired_schedules',
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+          data.forEach(function(schedule) {
+            print(schedule)
+            alert('Schedule from ' + schedule.origin + ' to ' + schedule.destination + ' has expired at ' + schedule.schetime);
+            // Customize the alert message or use a different way to notify the user
+          });
+        },
+        error: function(error) {
+          console.error('Error fetching expired schedules: ', error);
+        }
+      });
+    }, 6000); // Poll every minute (adjust as needed)
+  });
+  
